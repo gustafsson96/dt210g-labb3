8f8781de88f568b248c0aab4175c911af7a48ab3
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-11 15:47:35 +0100

    Display blog posts from backend to PostList page

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,9 +1,18 @@
-function PostItem() {
-  return (
-    <div>
-      
-    </div>
-  )
+import type { Post } from "../interfaces/Post";
+
+// Post prop
+interface Props {
+    post: Post;
+}
+
+function PostItem({ post }: Props) {
+    return (
+        <div>
+            <h2>{post.title}</h2>
+            <p>Av {post.author}</p>
+            <p>{post.content}</p>
+        </div>
+    );
 }
 
 export default PostItem;
--- src/pages/PostList.tsx
+++ src/pages/PostList.tsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "../services/postService";
+import type { Post } from "../interfaces/Post";
+import PostItem from "../components/PostItem";
+
 function PostList() {
-    return <h1>Post list</h1>;
+    // States to store posts, loading status and error messages
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    // Fetch data when component loads via postService
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getPosts();
+                // Set state with fetched posts
+                setPosts(data);
+            } catch (err) {
+                // Error message
+                setError("Kunde inte läsa in bloggposter.");
+            } finally {
+                // Set loading to false
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    // Show loading or error message if needed
+    // Replace with animation
+    if (loading) return <p>Läser in poster...</p>
+    if (error) return <p>Error</p>
+
+    return (
+        <>
+            <h1>Alla poster</h1>
+            {posts.length === 0 && <p>Ingen poster att visa.</p>}
+            {posts.map((post) => (
+                <PostItem key={post.id} post={post} />
+            ))}
+        </>
+    );
 }
 
 export default PostList;
