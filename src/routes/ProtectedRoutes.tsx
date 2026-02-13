a6846d50e2338add7bc7492e65d2fea3e2553b1f
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-13 17:16:22 +0100

    Update ProtectedRoutes component based on AuthContext file

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,5 +1,24 @@
-import Admin from "../pages/Admin";
+import { Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
+import { useAuth } from "../context/AuthContext";
 
-export const protectedRoutes = [
-    {path: "/admin", element: <Admin /> }
-];
+interface ProtectedRoutesProps {
+    children: ReactNode
+}
+
+const ProtectedRoutes: React.FC<ProtectedRoutesProps> = ({ children }) => {
+    // Get logged in user from AuthContext
+    const { user } = useAuth();
+
+    // Redirect to login if no user is logged in
+    if (!user) {
+        return <Navigate to="/login" replace />
+    }
+
+    // Render protected content if user is logged in
+    return (
+        <>{children}</>
+    )
+}
+
+export default ProtectedRoutes;
