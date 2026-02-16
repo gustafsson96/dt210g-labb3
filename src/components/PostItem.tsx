d5cbf07f78f1e31c060a7efeba48623d9cdc9e17
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-16 19:30:07 +0100

    Style PostItems in the post list

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,6 +1,7 @@
 import type { Post } from "../interfaces/Post";
 import { Link } from "react-router-dom";
- 
+import "./PostItem.css";
+
 // Post prop
 interface Props {
     post: Post;
@@ -10,13 +11,25 @@ function PostItem({ post }: Props) {
     // Excerpt to show only a part of a blog post
     const excerpt = post.content.length > 100 ? post.content.slice(0, 100) + "..." : post.content;
     return (
-        <div>
-            <h2>
-                <Link to={`/posts/${post.id}`}>{post.title}</Link>
-            </h2>
-            <p>{excerpt}</p>
-            <p>Av {post.author}</p>
-        </div>
+        <Link className="post-item-link" to={`/posts/${post.id}`}>
+            <div className="post-item">
+                <h2>
+                    {post.title}
+                </h2>
+                <p className="post-meta">
+                    <span>Av {post.author} |</span>
+                    <span>
+                        {new Date(post.created_at).toLocaleString(undefined, {
+                            year: "numeric",
+                            month: "short",
+                            day: "numeric",
+                            hour: "2-digit",
+                            minute: "2-digit"
+                        })}
+                    </span>
+                </p>
+                <p className="post-excerpt">{excerpt}</p>
+            </div></Link >
     );
 }
 
